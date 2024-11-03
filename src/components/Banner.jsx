6e24776375aaa83f04b8338379ec8b3c80c77a17
abut40052@gmail.com <abut40052@gmail.com>
2024-11-03 13:28:15 +0530

    Complate Live Link Surge

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,9 +2,7 @@ import React from "react";
 
 function Banner() {
   return (
-    <div className="hero bg-banner h-[82vh] bg-center bg-cover rounded-[16px]">
-      Banner
-    </div>
+    <div className="hero bg-banner h-[82vh] bg-center bg-cover rounded-[16px]"></div>
   );
 }
 
